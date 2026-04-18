d122d4d742c5663b49df207207d4d75338ba8ed5
Marci211 <zatrokmarton@gmail.com>
2026-04-18 07:55:13 +0200

    Add files via upload

--- proxy.js
+++ proxy.js
@@ -26,6 +26,31 @@ app.post('/api/v1/open/package/list', async (req, res) => {
     }
 });
 
+app.post('/api/email', async (req, res) => {
+    try {
+        const fetch = (await import('node-fetch')).default;
+        const response = await fetch('https://api.brevo.com/v3/smtp/email', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'api-key': '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'
+            },
+            body: JSON.stringify(req.body)
+        });
+        
+        if (response.ok) {
+            res.json({ success: true });
+        } else {
+            const errData = await response.json();
+            res.status(500).json({ success: false, error: errData });
+        }
+    } catch (error) {
+        console.error("Brevo Proxy Error:", error);
+        res.status(500).json({ success: false, errorMessage: "Email proxy error" });
+    }
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server and Proxy running on port ${PORT}`);
